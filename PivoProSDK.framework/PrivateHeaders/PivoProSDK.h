cf9a28d0198962ee7b81212229f32de8dcc7fccd
3i-ArlinKim <arlin.kim@3i.ai>
2024-10-28 09:58:43 +0900

    fix: hide horse tracking, add face tracking

--- PivoProSDK.framework/PrivateHeaders/PivoProSDK.h
+++ PivoProSDK.framework/PrivateHeaders/PivoProSDK.h
@@ -1,24 +1,21 @@
-\
-
 //
-//  PivoBasicSDK.h
-//  PivoBasicSDK
+//  PivoSDK.h
+//  PivoSDK
 //
-//  Created by Tuan on 2020/04/21.
+//  Created by Tuan on 2020/04/08.
 //  Copyright © 2020 3i. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for PivoBasicSDK.
-FOUNDATION_EXPORT double PivoBasicSDKVersionNumber;
+//! Project version number for PivoSDK.
+FOUNDATION_EXPORT double PivoSDKVersionNumber;
 
-//! Project version string for PivoBasicSDK.
-FOUNDATION_EXPORT const unsigned char PivoBasicSDKVersionString[];
+//! Project version string for PivoSDK.
+FOUNDATION_EXPORT const unsigned char PivoSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PivoBasicSDK/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <PivoSDK/PublicHeader.h>
 #import "NSData+SHA.h"
-#import "AuthWrapper.h"
 #import "KCFTrackerWrapper.h"
-
+#import "AuthWrapper.h"
 
--- PivoProSDK.podspec
+++ PivoProSDK.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "PivoProSDK"
-  spec.version      = "1.0.1"
+  spec.version      = "1.0.2"
   spec.summary      = "Pivo Pro SDK"
 
   # This description is used to generate tags and improve search results.
@@ -85,7 +85,7 @@ Pod::Spec.new do |spec|
   #  Supports git, hg, bzr, svn and HTTP.
   #
 
-  spec.source       = { :git => "https://github.com/pivo-inc/pivo-pro-sdk-ios.git", :tag => "1.0.1" }
+  spec.source       = { :git => "https://github.com/pivo-inc/pivo-pro-sdk-ios.git", :tag => "1.0.2" }
 
 
   # ――― Source Code ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
